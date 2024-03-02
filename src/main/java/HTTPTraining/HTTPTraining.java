0757ad10250f9f5b05a6de38ce549780ce8d30d9
PAndreev <pomaikai7378@gmail.com>
2024-03-02 11:48:27 +0300

    Commmit_11 Hemul_Many_Lines_corrected + MouseEvents

---
+++ src/main/java/HTTPTraining/HTTPTraining.java
@@ -0,0 +1,36 @@
+package HTTPTraining;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+
+public class HTTPTraining {
+
+    public static void main(String[] Args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://petstore.swagger.io/v2/pet/100501"))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+    System.out.println(response.statusCode());
+    System.out.println(response.body());
+
+
+        HttpClient client1 = HttpClient.newHttpClient();
+        HttpRequest request1 = HttpRequest.newBuilder()
+                .uri(URI.create("https://petstore.swagger.io/v2/pet"))
+                .headers("Content-Type", "application/json;charset=UTF-8")
+                .POST(HttpRequest.BodyPublishers.ofString("{\"id\":100501,\"category\":{\"id\":100500,\"name\":\"Molson1\"},\"name\":\"Molson1\",\"photoUrls\":[\"string\"],\"tags\":[{\"id\":100500,\"name\":\"Molson1\"}],\"status\":\"available\"}"))
+                .build();
+        HttpResponse<String> response1 = client1.send(request1, HttpResponse.BodyHandlers.ofString());
+
+
+        System.out.println(response1.statusCode());
+        System.out.println(response1.body());
+
+    }
+
+}
